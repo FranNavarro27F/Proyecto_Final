67201b3c18421bf2d19700a3bf56d2eee64c32fe
luisina gonzalez <luisinaggiacoia@gmail.com>
2022-09-09 12:01:14 -0300

    Creacion de componentes

---
+++ client/src/Components/DevUsers/DevUsers.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from '../DevUsers/DevUsers.module.css'
+
+export default function DevUsers() {
+  return (
+    <div>DevUsers</div>
+  )
+}
---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from '../Home/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
