07d87930d778275f9b82f655e19252111797efda
Francisco Navarro <fran27_navarro@hotmail.com>
2022-09-09 20:05:40 -0300

    cambios para ver

--- api/src/models/Contratos.js
+++ api/src/models/Contratos.js
@@ -24,7 +24,7 @@ module.exports = (sequelize) => {
     expiration_date:{
         type: DataTypes.DATE,
         allowNull: false,
-    },
+    }, //modifique!
     // id_dev:{
     //   type: DataTypes.STRING,
     //   allowNull: false,
