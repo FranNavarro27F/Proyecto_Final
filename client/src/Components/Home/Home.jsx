113e1fe00eda0b0cf0c33a78b8e6b28080c00821
Ramiro <ramiroferradas@hotmail.com>
2022-09-09 15:31:06 -0300

    arrancando el home

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import s from '../Home/Home.module.css'
+import React from "react";
+import s from "../Home/Home.module.css";
+import Girl1 from "./Assets/girl/girl1";
+import Circulo from "./Assets/circulo/circulo";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className={s.body}>
+      <div className={s.luz}></div>
+
+      <div className={s.container}>
+        <div className={s.circulo}>
+          <Circulo />
+        </div>
+        <div className={s.girl}>
+          <Girl1 />
+        </div>
+      </div>
+    </div>
+  );
 }
