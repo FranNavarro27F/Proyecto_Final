4afc8aef843798dfd3c65d6e624eaced5114b1e8
Ramiro <ramiroferradas@hotmail.com>
2022-09-10 04:15:40 -0300

    chica y diamantes

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import s from "./Home.module.css";
 import Girl1 from "./Assets/girl/girl1";
 
 import Landing from "../Landing/Landing";
-
+import diamante from "./Assets/Diamante/diamante.png";
 import Circulos from "./Assets/Circulos/Circulos";
 import Circulo from "./Assets/Circulo/Circulo";
 
@@ -12,8 +12,8 @@ export default function Home() {
     <div>
       <Landing />
       <div className={s.body}>
+        <div className={s.luz}></div>
         <div className={s.container}>
-          <div className={s.luz}></div>
           <div className={s.circulo1}>
             <Circulos />
           </div>
@@ -22,6 +22,10 @@ export default function Home() {
           </div>
           <div className={s.girl}>
             <Girl1 />
+          </div>
+          <div>
+            <img className={s.diamante} src={diamante} alt="diamante" />
+            <div className={s.luzDiamante}></div>
           </div>
         </div>
       </div>
