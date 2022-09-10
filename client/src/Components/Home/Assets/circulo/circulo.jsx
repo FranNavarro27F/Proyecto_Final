5f2acf3415844182cd98126eb01b2c1cc1bf0890
Ramiro <ramiroferradas@hotmail.com>
2022-09-10 01:23:24 -0300

    Avanzando en home

--- client/src/Components/Home/Assets/circulo/circulo.jsx
+++ client/src/Components/Home/Assets/circulo/circulo.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import s from "./circulo.module.css";
-import planetas from "../planetas/planetas.png";
+import s from "./Circulo.module.css";
 
 export default function Circulo() {
   return (
-    <div className={s.circulo}>
-      {/* <h1>HOLAAAAAA</h1> */}
-      {/* <img src={planetas} alt="" /> */}
-      <div className={s.elipse1432}></div>
-      <div className={s.elipse1431}></div>
-      <div className={s.elipse1430}></div>
-      <div className={s.elipse1429}></div>
-      <div className={s.elipse1423}></div>
+    <div className={s.body}>
+      <div className={s.circulo}>
+        <svg
+          width="620"
+          height="620"
+          viewBox="0 0 620 620"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            width="620"
+            height="620"
+            rx="310"
+            fill="url(#paint0_linear_39_160)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_39_160"
+              x1="82"
+              y1="94"
+              x2="582"
+              y2="620"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#C961DE" />
+              <stop offset="1" stop-color="#2954A3" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 }
