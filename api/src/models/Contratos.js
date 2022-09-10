510291ede959dcc01917ce60dc3c85538ccf0d8f
Santi O <s.ostrovsky@hotmail.com>
2022-09-10 16:24:00 -0300

    models-update

--- api/src/models/Contratos.js
+++ api/src/models/Contratos.js
@@ -1,23 +1,32 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes } = require("sequelize");
+// Exportamos una función que define el modelo
+// Luego le inyectamos la conexión a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('contratos', {
-    id:{
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
+  sequelize.define("contratos", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-      
+      type: DataTypes.STRING,
     },
-    expiration_date:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-
-
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    expiration_date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
 };
+
+/* RELACIONES
+
+CONTRATANTE
+CONTRATISTA
+
+
+*/
