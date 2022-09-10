837aefbf406a453c0ed3b621de5b12bf902c8a82
Santi O <s.ostrovsky@hotmail.com>
2022-09-10 16:13:41 -0300

    user-model-update

--- api/src/models/Usuarios.js
+++ api/src/models/Usuarios.js
@@ -1,20 +1,69 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes } = require("sequelize");
+// Exportamos una función que define el modelo
+// Luego le inyectamos la conexión a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('usuarios', {
-    id:{
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
+  sequelize.define("usuarios", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    name: {
+    profilePicture: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-
-
-
+    name: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    country: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING(80),
+      allowNull: true,
+    },
+    linkedIn: {
+      type: DataTypes.STRING(200),
+      allowNull: true,
+      unique: true,
+    },
+    gitHub: {
+      type: DataTypes.STRING(200),
+      allowNull: true,
+      unique: true,
+    },
+    webSite: {
+      type: DataTypes.STRING(200),
+      allowNull: true,
+      unique: true,
+    },
+    yearsOfExperience: {
+      type: DataTypes.INTEGER, // no permitir mas de dos dígitos
+      allowNull: false,
+    },
+    dailyBudget: {
+      type: Datatypes.INTEGER,
+      allowNull: true,
+    },
   });
 };
+
+/* RELACIONES
+    
+    servicios
+    lenguajes
+    tecnologías
+
+*/
