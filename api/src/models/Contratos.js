2724e20bfea52056125c9088cbc0162c107d20ff
Santi O <s.ostrovsky@hotmail.com>
2022-09-11 12:39:26 -0300

    update-modelo-contratos

--- api/src/models/Contratos.js
+++ api/src/models/Contratos.js
@@ -24,30 +24,27 @@ module.exports = (sequelize) => {
       type: DataTypes.DATE, // DATEONLY / STRING
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    creation_date: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    expiration_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
+    status: {
+      type: DataTypes.ENUM("Activo", "Inactivo", "Completado", "Cancelado"),
     },
   });
 };
 
-/* RELACIÓN
+// RELACIÓN
+// → ID CONTRATANTE
+// → ID CONTRATISTA
 
-CONTRATISTA id_dev:{
-   type: DataTypes.STRING,
-   allowNull: false,
- },
-CONTRATANTE id_client:{
-   type: DataTypes.STRING,
-   allowNull: false
- }
+// -----------------------------------------
 
- */
+// NOTAS
+
+// 1- Fechas → Es importante que las fechas de los contratos sean independientes de la fecha de creación, debido a que
+//             al existir una cola de contratos, los mismos pueden ser aceptados a futuro, y su fecha de inicio no sería
+//             la misma que su fecha de suscripción.
+
+// 2- Estado → Al existir una cola de contratos (y un registros de contratos en el perfil de cada usuario), es necesario que
+//             cada uno tenga un "Estado", que muestre la situación del mismo:
+//                 - Activo: El contrato se encuentra en ejecución, entre su fecha de inicio y fecha de finalización.
+//                 - Inactivo: Estado de los contratos suscritos a futuro, aquellos que todavía no han entrado en vigencia.
+//                 - Completado: Contrato cuya fecha de finalización ha acontecido y el trabajo ha sido entregado.
+//                 - Cancelado: Contrato que ha sido cancelado de común acuerdo entre el Desarrollador y el Contratante.
