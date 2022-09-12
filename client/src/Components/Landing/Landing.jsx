8488a96e5fdded6260efb233c40310604e35f749
Ramiro <ramiroferradas@hotmail.com>
2022-09-12 20:39:32 -0300

    home70%

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import s from '../Landing/Landing.module.css'
-import planetas1 from './Assets/planetas/planetas.png'
+import React from "react";
+import s from "../Landing/Landing.module.css";
+import planetas1 from "./Assets/planetas/planetas.png";
 
 export default function Landing() {
   return (
     <div className={s.landing}>
-        <div className={s.planetas}>
-          
-        <img className={s.imgPlanet} src={planetas1}></img> 
-            
-        </div>
-       
-        
-   
-
+      <div className={s.planetas}>
+        <img className={s.imgPlanet} src={planetas1}></img>
+      </div>
     </div>
-  )
+  );
 }
