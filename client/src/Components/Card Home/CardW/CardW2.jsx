ec0ad658c752f30d3fab7abd6eeddc148bb89236
luisina gonzalez <luisinaggiacoia@gmail.com>
2022-09-14 17:41:11 -0300

    creacionde CarHomeWork

---
+++ client/src/Components/Card Home/CardW/CardW2.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import s from './CardW2.module.css'
+
+export default function CardW2() {
+  return (
+    <div className={s.cardDiv2}>
+      <div className={s.cardw2}>
+<svg width="380" height="280" viewBox="0 0 380 280" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_b_39_329)">
+<rect width="380" height="280" rx="30" fill="url(#paint0_linear_39_329)" fill-opacity="0.4"/>
+<rect x="51" y="15" width="262" height="186" fill="url(#pattern0)"/>
+<rect x="0.5" y="0.5" width="379" height="279" rx="29.5" stroke="url(#paint1_linear_39_329)" stroke-opacity="0.3"/>
+</g>
+<defs>
+<filter id="filter0_b_39_329" x="-20" y="-20" width="420" height="320" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feGaussianBlur in="BackgroundImage" stdDeviation="10"/>
+<feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_39_329"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_39_329" result="shape"/>
+</filter>
+<pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+</pattern>
+<linearGradient id="paint0_linear_39_329" x1="190" y1="0" x2="190" y2="280" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="white" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint1_linear_39_329" x1="0" y1="0" x2="353" y2="280" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1"/>
+</linearGradient>
+</defs>
+</svg>
+</div>
+
+    </div>
+  )
+}
--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import Planeta from './Assets/Planeta/Planeta'
 import PlanetasChicos from './Assets/PlanetasChicos/PlanetasChicos'
 import ButtonLanding from './ButtonLanding/ButtonLanding'
 import SideMenu from './SideMenu/SideMenu'
+import s from './Landing.module.css'
 
 
 export default function Landing() {
@@ -13,7 +14,9 @@ export default function Landing() {
         <Planeta/>
         <SideMenu/>
         <ButtonLanding/>
-        
+          <div className={s.textLanding}>
+            <p className={s.text1landing}> Hazel Felix Ramos based in Tokushima Japan</p>
+            </div>     
    
     </div>
   );
