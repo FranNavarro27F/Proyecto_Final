c26fe5ca5c7f3422f97957ff1582391cdb1d79e5
duduesp <dudu.es@hotmail.com.ar>
2022-09-14 16:20:08 -0300

    creacion navbar y sus estilos. SIN TERMINAR

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,42 +1,61 @@
 import React from 'react'
 import s from '../NavBar/NavBar.module.css'
+import Select from 'react-select';
+import {HiOutlineSearch} from "react-icons/hi";
 
 export default function NavBar() {
+
+  const optionsTec = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+    { value: 'vanilla2', label: 'Vanilla' },
+    { value: 'vanilla3', label: 'Vanilla' },
+    { value: 'vanilla4', label: 'Vanilla' },
+    { value: 'vanilla5', label: 'Vanilla' },
+    { value: 'vanilla6', label: 'Vanilla' }
+  ]
+  const options = [
+    { value: 'web', label: 'Web Development' }, 
+    { value: 'strawberry', label: 'Web Design' },
+    { value: 'vanilla', label: 'UX/UI' }
+  ]
+  
   return (
     <header>
-    <div className={s.divGenNavBar}> 
-      <form>
+        <div className={s.divGen}>
+        <form>
         <input
         className={s.searchBar}
         type="text"
         placeholder='Search...'
-        ></input>
-        <button
-        className={s.buttonSearch}
-        type='submit'
         >
-          x
-        </button>
+        </input>
+          <HiOutlineSearch />
       </form>
-    </div>
-      <form>
-        <select
-        className={s.selectTecnologias}
-        >
-          <option>a</option>
-          <option>a</option>
-          <option>b</option>
-          <option>c</option>
-        </select>
-      </form>
-      <div
-      >
-        <p className={s.puntuacion}>Puntuación</p>
-      </div>
-      <div 
-      >
-        <p className={s.servicio}>Servicio</p>
-      </div>
+      <Select 
+        className={s.select}
+        isDisabled={false}
+        options={options}
+        isClearable={true}
+        isSearchable={true}
+        isMulti={true}
+        placeholder="Select a service"
+        
+      />
+      <Select 
+        className={s.select}
+        isDisabled={false}
+        options={optionsTec}
+        isClearable={true}
+        isSearchable={true}
+        isMulti={true}
+        placeholder="Select a tecnology"
+        
+      />
+
+        <button className={s.puntuacion}>Puntuación</button>
+        </div>
     </header>
   )
 }
--- client/src/Components/Work/Work.jsx
+++ client/src/Components/Work/Work.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Card from "./Card/Card";
 import s from "./Work.module.css";
 import { Link } from "react-router-dom";
+import NavBar from "../NavBar/NavBar"
 
 export default function Work() {
   return (
     <div className={s.body}>
+      <NavBar />
       <div className={s.cardsContainer}>
         <Link to={"urlll"} className={s.linkCard}>
           <Card
