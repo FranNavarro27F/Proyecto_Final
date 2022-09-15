2f9b700873de9ea82825d85a2460aa5b519a12fa
Santi O <s.ostrovsky@hotmail.com>
2022-09-15 12:35:18 -0300

    Task 002 - Ruta delete (servicios)

--- api/src/routes/Servicios/index.js
+++ api/src/routes/Servicios/index.js
@@ -1,28 +1,39 @@
 const { Router } = require("express");
-const { guardarServiciosEnDB, todosLosServicios } = require("../../controllers/Servicios");
+const {
+  guardarServiciosEnDB,
+  todosLosServicios,
+  deleteService,
+} = require("../../controllers/Servicios");
 const { Servicios } = require("../../db");
 
 const router = Router();
 
-const PATH = "/servicios";
+// ----------------------------------
 
-
-router.post('/', async (req, res)=>{
+router.post("/", async (req, res) => {
   try {
-    res.json(await guardarServiciosEnDB())
+    res.json(await guardarServiciosEnDB());
   } catch (e) {
-    res.status(400).json({error: e.message})
+    res.status(400).send(e);
   }
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     res.json(await todosLosServicios());
   } catch (e) {
-    res.status(400).json({error: e.message});
+    res.status(400).send(e);
   }
 });
 
-
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await deleteService(id);
+    res.send(`Activity deleted successfully`);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 module.exports = router;
