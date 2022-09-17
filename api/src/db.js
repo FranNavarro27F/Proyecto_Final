27400b470f49ecf5125a0d68ce24ccb8f828ee66
Francisco Navarro <fran27_navarro@hotmail.com>
2022-09-17 11:35:04 -0300

    bug-fix- db.js

--- api/src/db.js
+++ api/src/db.js
@@ -37,7 +37,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/finalproject`,
         {
           logging: false, // set to console.log to see the raw SQL queries
           native: false, // lets Sequelize know we can use pg-native for ~30% more speed
