b01902ccec8b98fdfb167b9226333a67dab35e26
duduesp <dudu.es@hotmail.com.ar>
2022-09-19 15:19:45 -0300

    Solucionado error de validaciones.

--- client/src/Components/DevUsersCreate/Validaciones.jsx
+++ client/src/Components/DevUsersCreate/Validaciones.jsx
@@ -2,19 +2,19 @@ function validaciones(input) {
   const errors = {};
   //name
   if (input.name === "") errors.name = "Ingresa tu nombre!";
-  if (/[^\w\s]/.test(input.name))
+  else if (/[^\w\s]/.test(input.name))
     errors.name = "El nombre no debe contener caracteres especiales!";
-  if (/[1-9]/.test(input.name))
+  else if (/[1-9]/.test(input.name))
     errors.name = "El nombre no debe contener numeros!";
-  if (/[\s]/.test(input.name))
+  else if (/[\s]/.test(input.name))
     errors.name = "El nombre no debe contener espacios!";
   //lastName
-  if (input.lastName === "") errors.lastName = "Ingresa tu apellido!";
-  if (/[^\w\s]/.test(input.lastName))
+  else if (input.lastName === "") errors.lastName = "Ingresa tu apellido!";
+  else if (/[^\w\s]/.test(input.lastName))
     errors.lastName = "El apellido no debe contener caracteres especiales!";
-  if (/[1-9]/.test(input.lastName))
+  else if (/[1-9]/.test(input.lastName))
     errors.lastName = "El apellido no debe contener numeros!";
-  if (/[\s]/.test(input.lastName))
+  else if (/[\s]/.test(input.lastName))
     errors.lastName = "El apellido no debe contener espacios!";
   //image
   // if (input.profilePicture === "")
@@ -24,68 +24,68 @@ function validaciones(input) {
   // if (!/\.(jpg|png|gif)$/i.test(input.profilePicture))
   //   errors.profilePicture = "El url que intentas colocar no es valida";
   //email
-  if (input.email === "") errors.email = "Ingresa tu email!";
-  if (/[\s]/.test(input.email))
+  else if (input.email === "") errors.email = "Ingresa tu email!";
+  else if (/[\s]/.test(input.email))
     errors.email = "El email no debe contener espacios!";
-  if (!/^\S+@\S+\.\S+$/.test(input.email))
+    else if (!/^\S+@\S+\.\S+$/.test(input.email))
     errors.email = "El email que intentas colocar no es valido";
   //linkedin
-  if (input.linkedIn === "") errors.linkedIn = "Ingresa la url de tu linkedin!";
-  if (/[\s]/.test(input.linkedIn))
+  else if (input.linkedIn === "") errors.linkedIn = "Ingresa la url de tu linkedin!";
+  else if (/[\s]/.test(input.linkedIn))
     errors.linkedIn = "El url no debe contener espacios!";
 
-  if (!/linkedin/i.test(input.linkedIn))
+    else if (!/linkedin/i.test(input.linkedIn))
     errors.linkedIn = "Ingresa una url de LinkedIn correcta!";
-  if (
+    else if (
     !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(
       input.linkedIn
     )
   )
     errors.linkedIn = "Ingresa una url correcta!";
   //GitHub
-  if (input.gitHub === "") errors.gitHub = "Ingresa la url de tu gitHub!";
-  if (/[\s]/.test(input.gitHub))
+  else if (input.gitHub === "") errors.gitHub = "Ingresa la url de tu gitHub!";
+  else if (/[\s]/.test(input.gitHub))
     errors.gitHub = "El url no debe contener espacios!";
-  if (
+    else if (
     !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(
       input.gitHub
     )
   )
     errors.gitHub = "Ingresa una url correcta!";
-  if (!/github/i.test(input.gitHub))
+    else if (!/github/i.test(input.gitHub))
     errors.gitHub = "Ingresa una url de GitHub correcta!";
 
   //Website
-  if (input.webSite === "") errors.webSite = "Ingresa la url de tu webSite!";
-  if (/[\s]/.test(input.webSite))
+  else if (input.webSite === "") errors.webSite = "Ingresa la url de tu webSite!";
+  else if (/[\s]/.test(input.webSite))
     errors.webSite = "El url no debe contener espacios!";
 
-  if (
+    else if (
     !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(
       input.webSite
     )
   )
     errors.webSite = "El url que intentas colocar no es valida";
   //yearsOfExperience
-  if (input.yearsOfExperience === 0)
+  else if (input.yearsOfExperience === 0)
     errors.yearsOfExperience = "El valor no puede ser 0!";
-  if (input.yearsOfExperience > 99 || input.yearsOfExperience < 1)
+    else if (input.yearsOfExperience > 99 || input.yearsOfExperience < 1)
     errors.yearsOfExperience = "El valor debe estar entre 1 y 99";
   //dailyBudget
-  if (input.dailyBudget === 0) errors.dailyBudget = "El valor no puede ser 0!";
-  if (input.dailyBudget > 999 || input.dailyBudget < 1)
+  else if (input.dailyBudget === 0) errors.dailyBudget = "El valor no puede ser 0!";
+  else if (input.dailyBudget > 999 || input.dailyBudget < 1)
     errors.dailyBudget = "El valor debe estar entre 1 y 999";
 
   //countrie
-  if (!input.paiseId?.length) errors.countries = "Ingresa un pais!";
+  else if (!input.paiseId?.length) errors.countries = "Ingresa un pais!";
   // tecnologias;
-  if (!input.tecnologias?.length)
+  else if (!input.tecnologias?.length)
     errors.tecnologies = "Ingresa al menos una tecnologia!";
   //lenguajes
-  if (!input.lenguajes?.length)
+  else if (!input.lenguajes?.length)
     errors.languajes = "Ingresa al menos un lenguaje!";
   //servicios
-  if (!input.servicios?.length)
+  else if (!input.servicios?.length)
     errors.services = "Ingresa al menos un servicio!";
 
   return errors;
