29dd9193377cc6bc80062959a1b4d9b8280f9b53
Ramiro <ramiroferradas@hotmail.com>
2022-09-20 02:34:23 -0300

    renderizado condicional de nombre en el home

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -4,11 +4,15 @@ import PlanetasChicos from "./Assets/PlanetasChicos/PlanetasChicos";
 import ButtonLanding from "./ButtonLanding/ButtonLanding";
 import SideMenu from "./SideMenu/SideMenu";
 import s from "./Landing.module.css";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, User } from "@auth0/auth0-react";
 
 export default function Landing() {
   const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   // console.log(user);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <PlanetasChicos />
@@ -17,7 +21,8 @@ export default function Landing() {
       <ButtonLanding />
       <div className={s.textLanding}>
         <p className={s.text1landing}>
-          Bienvenido a <span className={s.programax}>Programax</span>
+          Bienvenido {user?.given_name} a{" "}
+          <span className={s.programax}>Programax</span>
           <i>!</i>
         </p>
         <p className={s.text1landing2}>
