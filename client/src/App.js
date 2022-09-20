7477f36c5f15423018e9556a7547686b8cafd8f2
luisina gonzalez <luisinaggiacoia@gmail.com>
2022-09-20 20:09:12 -0300

    stripe

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-import { YOUR_CLIENT_ID } from "./Components/Paypal/ClientID";
+// import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+// import { YOUR_CLIENT_ID } from "./Components/Paypal/ClientID";
 
 //componentes
 import Details from "./Components/Details/Details";
@@ -9,12 +9,14 @@ import Users from "./Components/Users/Users";
 import Work from "./Components/Work/Work";
 import DevUsersCreate from "./Components/DevUsersCreate/DevUsersCreate";
 import Profile from "./Components/Login/UserProfile/Profile";
+import Stripe from "stripe";
 
 function App() {
   return (
     <>
       <Routes>
-        <PayPalScriptProvider options={{ "client-id": YOUR_CLIENT_ID }} />
+        <Route path ='/checkout' element={<Stripe/>}></Route>
+        {/* <PayPalScriptProvider options={{ "client-id": YOUR_CLIENT_ID }} /> */}
         <Route path="/" element={<Home />} />
         <Route path="/work/details/:id" element={<Details />} />
         <Route path="/profile" element={<Profile />} />
--- client/src/Components/Paypal/ClientID.js
+++ client/src/Components/Paypal/ClientID.js
@@ -1,2 +1,2 @@
-export const YOUR_CLIENT_ID =
-  "AeHR7NTh557kPnX21CUjtKOuiw1PZ_-qd6k6zOj8DnkISGCzorU_BFGccp2DHhodvVTFOBnLVxO62ANW";
+// export const YOUR_CLIENT_ID =
+//   "AeHR7NTh557kPnX21CUjtKOuiw1PZ_-qd6k6zOj8DnkISGCzorU_BFGccp2DHhodvVTFOBnLVxO62ANW";
