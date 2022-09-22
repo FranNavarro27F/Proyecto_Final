dd640cc8897660d09140cd5a610adb02f285795b
roberto <roberbaldovino167@gmail.com>
2022-09-22 09:19:02 -0500

    buda en false

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ const { saveLanguages } = require('./src/controllers/Lenguajes/index')
 
 // Syncing all the models at once.
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false}).then(() => {
   const PORT= 3001;
 
   server.listen(process.env.PORT || PORT, async () => {
