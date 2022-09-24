09e6d24fef788ed49b24376ae8845d8819363805
Francisco Navarro <fran27_navarro@hotmail.com>
2022-09-24 11:23:31 -0300

    restaurando emailer pisado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,7 +7,8 @@ const tecnologiasRouter = require("../routes/Tecnologias/index.js");
 const paisesRouter = require("../routes/Paises/index.js");
 const serviciosRouter = require("../routes/Servicios/index.js");
 const contratosRouter = require("../routes/Contratos/index.js");
-const stripeRouter = require ('../routes/Stripe/Stripe')
+const stripeRouter = require ('../routes/Stripe/Stripe');
+const emailerRouter= require("../routes/Emailer/index.index");
 
 const router = Router();
 
@@ -20,5 +21,7 @@ router.use("/paises", paisesRouter);
 router.use("/servicios", serviciosRouter);
 router.use("/contratos", contratosRouter);
 router.use ('/checkout', stripeRouter);
+router.use ("/emailer", emailerRouter);
+
 
 module.exports = router;
