ada4d43b53a6db090ed91506d44dc32971b25617
luisina gonzalez <luisinaggiacoia@gmail.com>
2022-09-25 13:15:17 -0300

    stripe

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Users from "./Components/UserProfile/Users.jsx";
 import Work from "./Components/Work/Work";
 import DevUsersCreate from "./Components/DevUsersCreate/DevUsersCreate";
 //import Profile from "./Components/Login/UserProfile/Profile";
-import Stripe from "./Components/Stripe/Stripe";
+import Stripe from "./Components/Stripe/Stripe.js";
 import UserProfile from "./Components/UserProfile/UserProfile";
 //import About from "./Components/About/About"
 import Contracts from "./Components/Contracts/Contracts";
