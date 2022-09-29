668a961a04cd484d3a276230db181efc78f32fd1
Santi O <s.ostrovsky@hotmail.com>
2022-09-29 20:42:09 -0300

    organizacion back

--- api/src/controllers/Paises/index.js
+++ api/src/controllers/Paises/index.js
@@ -1,11 +1,12 @@
 const { Paises } = require("../../db");
 const { paises } = require("../../json/Paises.json");
 
-const ERROR = "Error @ controllers/Countries";
+const ERROR = "Error @ controllers/Countries/";
 
 // -----------------------------------------------
 
 const jsonPaises = async () => {
+  //
   try {
     paises.map(async (name) => {
       await Paises.findOrCreate({
@@ -13,45 +14,62 @@ const jsonPaises = async () => {
       });
     });
     console.log("✔ Paises cargados.");
+    return "✔ Paises cargados.";
+    //
   } catch (e) {
-    console.error(`ERROR @ controllers/jsonPaises --→ ${e}`);
+    console.error(`${ERROR}jsonPaises --→ ${e}`);
   }
 };
 
+// -----------------------------------------------
+
 const getPaises = async () => {
+  //
   try {
     datosPais = await Paises.findAll();
     return datosPais;
+    //
   } catch (e) {
-    console.error(`ERROR @ controllers/getPaises --→ ${e}`);
+    console.error(`${ERROR}getPaises --→ ${e}`);
   }
 };
 
+// -----------------------------------------------
+
 const postPaises = async (name) => {
+  //
   try {
     datosPais = await Paises.findOrCreate({
       where: {
         name,
       },
     });
+    //
   } catch (e) {
-    console.error(`ERROR @ controllers/postPaises --→ ${e}`);
+    console.error(`${ERROR}postPaises --→ ${e}`);
   }
 };
 
+// -----------------------------------------------
+
 const deletePaises = async (id) => {
+  //
   try {
     await Paises.destroy({
       where: { id },
     });
     console.log(`Country(${id}) deleted successfully`);
     return `Country (${id}) deleted successfully`;
+    //
   } catch (e) {
-    console.error(`ERROR @ controllers/deletePaises --→ ${e}`);
+    console.error(`${ERROR}deletePaises --→ ${e}`);
   }
 };
 
+// -----------------------------------------------
+
 const putPaises = async (id, name) => {
+  //
   try {
     await Paises.update(
       { name },
@@ -61,19 +79,26 @@ const putPaises = async (id, name) => {
     );
     console.log(`Country (${id}) updated successfully`);
     return `Country (${id}) updated successfully`;
+    //
   } catch (e) {
-    console.error(`ERROR @ controllers/putPaises --→ ${e}`);
+    console.error(`${ERROR}putPaises --→ ${e}`);
   }
 };
 
+// -----------------------------------------------
+
 const traerPaisPorId = async (id) => {
+  //
   try {
     let paisPorId = await Paises.findByPk(id);
     return paisPorId;
+    //
   } catch (e) {
-    console.error(`ERROR @ controllers/traerPaisPorId --→ ${e}`);
+    console.error(`${ERROR}traerPaisPorId --→ ${e}`);
   }
 };
+
+// -----------------------------------------------
 
 module.exports = {
   getPaises,
