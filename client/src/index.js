9d33d9fa9c64a742e5480293ac5d5d2cc662999c
Francisco Navarro <fran27_navarro@hotmail.com>
2022-09-30 16:24:39 -0300

    restaurado trabajo pisado2

--- client/src/index.js
+++ client/src/index.js
@@ -12,12 +12,9 @@ import dotenv from "dotenv";
 // import "bootswatch/dist/";
 
 dotenv.config();
-<<<<<<< HEAD
 
 console.log("*************************",process.env.REACT_APP_BACKEND_URL,"**********************")
 
-=======
->>>>>>> 0bb44fe421f5ac759c62b737a70675ed90a8661d
 
 axios.defaults.baseURL =
   process.env.REACT_APP_API ||
