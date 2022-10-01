5db0ba7abf301449a79e5c53cfeb49d12c6711c1
Ramiro <ramiferra97@gmail.com>
2022-10-01 20:46:46 -0300

    update form

--- client/src/Components/Details/Details.jsx
+++ client/src/Components/Details/Details.jsx
@@ -499,8 +499,6 @@ export default function Details() {
                     {/* [0].toUpperCase()+ userDetail.name.slice(1) + ' '//[0].toUpperCase()+ userDetail.lastName.slice(1)} */}
                     <br />
                     <div className={s.imageBox}>
-                      {/* <img >{userDetail?.profilePicture}</img> */}
-
                       {userDetail?.profilePicture &&
                       userByEmail?.profilePicture ? (
                         <img
@@ -524,6 +522,20 @@ export default function Details() {
                         >
                           <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"></path>
                         </svg>
+                      )}
+                      {userByEmail && userByEmail?.postulado && (
+                        <div className={s.divVisible}>
+                          <label htmlFor="">Hacer visible mi postulacion</label>
+                          <label className={s.switch}>
+                            <input
+                              onChange={(e) => handleVisible(e)}
+                              type="checkbox"
+                              name="visible"
+                              defaultChecked={userByEmail?.visible}
+                            />
+                            <span className={s.slider_round}></span>
+                          </label>
+                        </div>
                       )}
                     </div>
                     <br />
@@ -649,17 +661,6 @@ export default function Details() {
                     </button>
                     {userProfile ? (
                       <div className={s.buttonsLogeado}>
-                        {userByEmail && (
-                          <label className={s.switch}>
-                            <input
-                              onChange={(e) => handleVisible(e)}
-                              type="checkbox"
-                              name="visible"
-                              defaultChecked={userByEmail?.visible}
-                            />
-                            <span className={s.slider_round}></span>
-                          </label>
-                        )}
                         <button
                           className={s.buttonBack}
                           onClick={() => navigate("/create")}
