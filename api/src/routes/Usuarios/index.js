135030f79a7bf494ee38c8c5296c599f6c9859b2
Santi O <s.ostrovsky@hotmail.com>
2022-10-02 17:25:27 -0300

    update rutas usuarios

--- api/src/routes/Usuarios/index.js
+++ api/src/routes/Usuarios/index.js
@@ -89,11 +89,14 @@ router.get("/:id", async (req, res) => {
 
 router.put("/", async (req, res) => {
   try {
-   res.status(200).json(await modifyUser(req.body));
+    res.status(200).json(await modifyUser(req.body));
   } catch (e) {
     res.status(400).send(`Error --→ ${e}`);
   }
 });
+
+// -----------------------------------------------
+
 // Set SUBSCRIPTION ID
 router.put("/sub/:id", async (req, res) => {
   //
